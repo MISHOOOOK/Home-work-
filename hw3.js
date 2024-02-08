229d6c8bc0e7ce33b1709373819420b559ebb483
MISHOOOOK <butenko.misha006@gmail.com>
2024-02-08 12:19:59 +0100

    готово

--- hw3.js
+++ hw3.js
@@ -1,7 +1,7 @@
 function checkProbabilityTheory(count) {
     let evenCount = 0, oddCount = 0;
     for (let i = 0; i < count; i++) {
-        let num = Math.floor(Math.random() * 900) + 100;
+        let num = Math.floor(Math.random() * 901) + 100;
         num % 2 === 0 ? evenCount++ : oddCount++;
     }
     let evenPercentage = (evenCount / count) * 100;
@@ -10,6 +10,15 @@ function checkProbabilityTheory(count) {
     console.log(`Парних чисел: ${evenCount}`);
     console.log(`Непарних чисел: ${oddCount}`);
     console.log(`Відсоток парних до не парних: ${evenPercentage.toFixed(2)}% : ${oddPercentage.toFixed(2)}%`);
-    console.log('____________________________________________________________');
+    console.log('_'.repeat(60));
 }
+checkProbabilityTheory(100);
+checkProbabilityTheory(200);
+checkProbabilityTheory(300);
+checkProbabilityTheory(400);
+checkProbabilityTheory(500);
+checkProbabilityTheory(600);
+checkProbabilityTheory(700);
+checkProbabilityTheory(800);
+checkProbabilityTheory(900);
 checkProbabilityTheory(1000);
