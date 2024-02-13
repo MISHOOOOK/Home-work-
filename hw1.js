ff6976e93ef12d7d0af2ed59020e67a701705ec6
MISHOOOOK <butenko.misha006@gmail.com>
2024-02-13 17:33:07 +0100

    let user 6 властивостей

--- hw1.js
+++ hw1.js
@@ -1,15 +1,16 @@
-function newIsNaNFunction(value) {
-    // Перевіряємо, чи є вхідне значення не числом
-    if (typeof value !== 'number') {
-        return true;
-    }
-    return value !== value;
+let user = {
+    name : "Misha",
+    age : 30,
+    student : "yes",
+    sName : "Butenko",
+    yerOfStud : "first",
+    gender : 'male',
+    
+
+
+
+
+
+
+
 }
-// Приклади 
-console.log(newIsNaNFunction(NaN)); 
-console.log(newIsNaNFunction(123)); 
-console.log(newIsNaNFunction('abc'));
-console.log(newIsNaNFunction(undefined)); 
-console.log(newIsNaNFunction(false));
-console.log(newIsNaNFunction(true)); 
-console.log(newIsNaNFunction('21414')); 
