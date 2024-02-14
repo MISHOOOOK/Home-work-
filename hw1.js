0e1db8397b99ba1262e9601946df696b0f8f9682
MISHOOOOK <butenko.misha006@gmail.com>
2024-02-14 20:41:32 +0100

    new file and var + function + for + const

---
+++ hw1.js
@@ -0,0 +1,12 @@
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]; 
+function myBlend(arr){
+    for(let h = arr.length - 1; h > 0; h++){
+        const g = Math.floor(Math.random() * (h + 1)); 
+
+
+    }
+
+
+
+
+}
