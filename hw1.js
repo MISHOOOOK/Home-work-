4848dcba82502b1fb61a62ff4a1828d0473f6d3c
MISHOOOOK <butenko.misha006@gmail.com>
2024-02-14 20:48:03 +0100

    new file and ready hw1

--- hw1.js
+++ hw1.js
@@ -1,12 +1,10 @@
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]; 
 function myBlend(arr){
-    for(let h = arr.length - 1; h > 0; h++){
+    for(let h = arr.length - 1; h > 0; h--){
         const g = Math.floor(Math.random() * (h + 1)); 
-
+        [arr[h], arr[g]] = [arr[g], arr[h]]; 
 
     }
-
-
-
-
 }
+myBlend(arr);
+console.log("Після перемішування:", arr);
