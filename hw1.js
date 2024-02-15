6bdd1fb2d65a62a2b73faf0dfb0fbac5f3abc8da
MISHOOOOK <butenko.misha006@gmail.com>
2024-02-15 21:39:33 +0100

    ready

---
+++ hw1.js
@@ -0,0 +1,16 @@
+    let strings = [
+        "sdfpssjfksdfj",    
+        "fjskdfjskdjaaaaaaa",   
+        "254498sfsf878787",   
+        "afdsadsaaaaaaaaaaaaaa",  
+        "2658845238456+",     
+        "ds4f5sdf98sdf7sfa6f54"     
+    ];
+      let RE = /^[^Aa]{6,}$/;
+       strings.forEach(str => {
+        if (RE.test(str)) {
+            console.log(`"${str}" Відповідає умовам`);
+        } else {
+            console.log(`"${str}" Не відповідає умовам`);
+        }
+    });
